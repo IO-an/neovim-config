a65a4d8808057c0e4ed9cb1608333f5981e5bc4f
Ivan Vasiliev <va3iliev.ip@gmail.com>
2026-05-31 23:13:50 +0300

    Fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,24 @@
--- Tree-sitter – парсер, который строит синтаксическое дерево и даёт точную
--- подсветку, структурную навигацию и отступы.
+-- Дерево синтаксического разбора – точная подсветка и отступы.
+-- Использует новое API nvim-treesitter (без nvim-treesitter.configs).
 return {
   'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',          -- обновляет парсеры после установки
-  event = 'VeryLazy',           -- загружается без блокировки стартового экрана
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        'html', 'css', 'scss', 'less',
-        'javascript', 'typescript', 'tsx',
-        'json', 'yaml',
-        'c', 'cpp', 'glsl',
-        'ruby', 'python', 'go', 'rust',
-        'bash',
-        'java', 'c_sharp', 'scala',
-        'pascal',
-        'lua',
-        'styled',  -- styled-components
-        'dotenv',  -- .env файлы
-      },
-      highlight = { enable = true },   -- включаем подсветку Tree-sitter
-      indent = { enable = true },      -- включаем отступы по синтаксису
-    })
-  end,
+  build = ':TSUpdate',
+  event = 'VeryLazy',
+  opts = {
+    ensure_installed = {
+      'html', 'css', 'scss', 'less',
+      'javascript', 'typescript', 'tsx',
+      'json', 'yaml',
+      'c', 'cpp', 'glsl',
+      'ruby', 'python', 'go', 'rust',
+      'bash',
+      'java', 'c_sharp', 'scala',
+      'pascal',
+      'lua',
+      'styled',  -- styled-components
+      'dotenv',  -- .env
+    },
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
