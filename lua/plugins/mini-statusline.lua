89256399cf5662309fa576b86fa4722bf56d5111
Ivan Vasiliev <va3iliev.ip@gmail.com>
2026-05-31 23:03:49 +0300

    Fix mini statusline again

--- lua/plugins/mini-statusline.lua
+++ lua/plugins/mini-statusline.lua
@@ -5,20 +5,18 @@ return {
   event = 'VeryLazy',
   config = function()
     local statusline = require('mini.statusline')
-    local diagnostics = require('mini.statusline').diagnostics  -- модуль диагностики
 
     statusline.setup({
       use_icons = false,
       set_vim_settings = false,
+
       -- Функция для активной статус-строки (возвращает строку)
       content = {
         active = function()
           local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-          local file = MiniStatusline.section_file({ trunc_width = 140 })
+          local file = vim.fn.expand('%:t')  -- имя файла
           local diag = MiniStatusline.section_diagnostics({
             symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
-            -- nil для отображения количества всех severity
-            -- Можно оставить все, чтобы видеть и предупреждения
             options = { count = true },
           })
           local location = MiniStatusline.section_location()
